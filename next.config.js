ae06ef50007e0605a038be2c0cc49f6ba747c6e1
luka <luukitto@gmail.com>
2026-02-10 01:25:58 +0400

    Fix next.config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const basePath = isGithubPages && repo ? `/${repo}` : "";
-const assetPrefix = basePath ? `${basePath}/` : undefined;
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "NG-studio";
+const basePath = isGithubPages ? `/${repo}` : "";
+const assetPrefix = isGithubPages ? `${basePath}/` : undefined;
 
 const nextConfig = {
   output: "export",
