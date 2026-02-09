127f6ee2353781383a87e24e6ed2b4d904784075
luka <luukitto@gmail.com>
2026-02-10 01:32:48 +0400

    Fix unused import for GitHub build

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { motion } from "framer-motion";
 import { HERO_IMAGE, LINKS } from "@/lib/constants";
 import { useLanguage } from "@/components/LanguageProvider";
